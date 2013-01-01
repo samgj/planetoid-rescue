104c214419f77e19bfdd178330a3a29af6d85ba3
Sam Jackson <samahstan@gmail.com>
2013-01-01 09:14:27 -0600

    Added leagal notice to collisions.h

--- src/collisions.h
+++ src/collisions.h
@@ -1,4 +1,23 @@
-#include <SDL.h>
+//Source code for Planetoid Rescue from Unincorporated Games
+
+//Copyright 2012 Unincorporated Media
+
+//This file is part of Planetoid Rescue
+
+//Planetoid Rescue if free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//Planetoid Rescue is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PATICULAR PURPOSE. See the
+//GNU General Public License for more details.
+
+//You should have recieved a copy of the GNU General Public License
+//along with Planetoid Rescue. If not see <http://www.gnu.org/licenses/>.
+
+#include <math.h>
 
 float distance( float a[3], float b[3] ) //use the loc arrays for objects
 {
