c6838c801867e6c30b2461ab1b07aa1e3200e7fa
Sam Jackson <samahstan@gmail.com>
2013-05-11 08:05:33 -0500

    Added collisions.h to objects.h

--- src/objects.h
+++ src/objects.h
@@ -19,6 +19,7 @@
 //You should have received a copy of the GNU General Public License
 //along with Planetoid Rescue.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "collisions.h"
 
 class ai
 {
